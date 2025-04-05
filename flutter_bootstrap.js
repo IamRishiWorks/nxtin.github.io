006c9f0ce5e14d154ac098c08b369fa6b2388c2d
Jagadishwar <je@hirekart.io>
2025-04-05 11:24:05 +0530

    updated sizes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2734839501"
+    serviceWorkerVersion: "1385528684"
   }
 });
