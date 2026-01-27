e08d31931d754d9d844b1de6423d2aa99f568021
Jagadishwar Enagurthi <157389544+IamRishiWorks@users.noreply.github.com>
2026-01-28 01:13:59 +0530

    new changes added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "40bbaf9e801b8c1013b47d7a41ac5e53",
+const RESOURCES = {"flutter_bootstrap.js": "bf0526923c3ba21c95876f6f3e6fbe31",
 "version.json": "d8d11acd2db2fd09f38f4a1263201876",
 "favicon.ico": "901e776ed637cd169c45fc54ac7d4b2a",
 "index.html": "f7c812471b28127c5db8260c8f7ec4f9",
 "/": "f7c812471b28127c5db8260c8f7ec4f9",
 "web-app-manifest-192x192.png": "795aa6767a5315ec30e74bd2220100a7",
 "apple-touch-icon.png": "7b8ef9d06b9f6b172107a998079017bc",
-"main.dart.js": "171de1d26642dd56d6eb69ff8f9f068e",
+"main.dart.js": "2db649bdc52c8bdcbcc4bf8b61aec1cf",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "web-app-manifest-512x512.png": "283c8957e92fe4cc6b296c7591675764",
